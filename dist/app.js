79191477842ce892b5f8cc9f482ba561794cfc38
damian.ruczynski <damian.ruczynski@britenet.com.pl>
2022-09-21 12:16:21 +0200

    rendering form

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,14 @@
 "use strict";
-console.log('HEJ DZIALAM');
+class ProjectInput {
+    constructor() {
+        this.templateElement = document.getElementById('project-input');
+        this.hostElement = document.getElementById('app');
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.formToAttach = importedNode.firstElementChild;
+        this.attachContent();
+    }
+    attachContent() {
+        this.hostElement.insertAdjacentElement('afterbegin', this.formToAttach);
+    }
+}
+const newProj = new ProjectInput;
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,23 @@
-console.log('HEJ DZIALAM');
+class ProjectInput {
+    templateElement: HTMLTemplateElement
+    hostElement: HTMLDivElement
+    formToAttach: HTMLFormElement
+
+    constructor() {
+        this.templateElement = <HTMLTemplateElement>document.getElementById('project-input')!
+        this.hostElement = <HTMLDivElement>document.getElementById('app')!
+
+        const importedNode = document.importNode(this.templateElement.content, true)
+        this.formToAttach = <HTMLFormElement>importedNode.firstElementChild
+        this.attachContent()
+    }
+
+    private attachContent() {
+        this.hostElement.insertAdjacentElement('afterbegin', this.formToAttach)
+    }
+
+}
+
+const newProj = new ProjectInput
+
+
